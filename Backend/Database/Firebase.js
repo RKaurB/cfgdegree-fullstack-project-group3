0871b82f24d874b30ae4fc136beee0f4db294228
omanoma <omanoma14@gmail.com>
2026-06-06 16:53:07 +0100

    Add Firebase for Loging and Create

---
+++ Backend/Database/Firebase.js
@@ -0,0 +1,53 @@
+import { initializeApp } from 'firebase/app';
+import {getAuth,createUserWithEmailAndPassword, signInWithEmailAndPassword }from 'firebase/auth'
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+require('dotenv').config()
+
+class FirebaseDB{
+    #db;
+    #auth;
+    constructor(){
+      const firebaseConfig = {
+            apiKey: `${process.env.ApiKey}`,
+            authDomain: "fullstackgroup3-610c2.firebaseapp.com",
+            projectId: "fullstackgroup3-610c2",
+            storageBucket: "fullstackgroup3-610c2.firebasestorage.app",
+            messagingSenderId: "667266312602",
+            appId: `${process.env.AppID}`,
+            measurementId: `${process.env.MeasurementId}`
+        }   
+        const app = initializeApp(firebaseConfig);
+        this.#db = getFirestore(app)
+        this.#auth = getAuth(app)
+    }
+
+    async createUserUsingEmail(email,password) {
+        try{
+            const userCredential = await createUserWithEmailAndPassword(this.#auth,email,password);
+            return {user:userCredential.user}
+        }catch(error){
+            return {
+                errorCode: error?.code,
+                errorMessage:error?.message
+            }
+        }
+    }
+    async checkIfUserIsSign(email){
+        const user = await this.#auth.currentUser
+        if(user == null) return false;
+        return (user.email === email);
+    }
+    async loginUserUsingEmail(email,password){
+
+        try{
+            const userCredential = await signInWithEmailAndPassword(this.#auth,email,password);
+            return {user:userCredential?.user}
+        }catch(error){
+            return {
+                errorCode: error?.code,
+                errorMessage:error?.message
+            }
+        }
+    }
+            
+}
