fde88bccbc200fa99828c587a6b5e1008da47c79
juliavolponi <juliadvolponi@gmail.com>
2026-06-07 20:09:33 +0100

    applied requirement for empty input

--- Frontend/src/pages/LandingPage.jsx
+++ Frontend/src/pages/LandingPage.jsx
@@ -1,38 +1,88 @@
-import Button from "../components/Button"
+import Button from "../components/Button";
 
 function LandingPage() {
-        return (<div className="p-4">
-            <div className="hero">
-                <h1>Garden Buddy</h1>
-                <p>Your personal garden planner — discover plants, build your garden and never miss a watering day.</p>
-            </div>
+  return (
+    <div className="p-4">
+      <div className="hero">
+        <h1>Garden Buddy</h1>
+        <p>
+          Your personal garden planner — discover plants, build your garden and
+          never miss a watering day.
+        </p>
+      </div>
 
-            <div className="container">
-                <div className="row mt-5 justify-content-center">
-                    <div className="col auth-form h-100">
-                        <h2>Login</h2>
-                            <label>Email</label>
-                            <input type="email" name="email" id="email" placeholder="Type your email" className="form-control mb-2"/>
-                            <label>Password</label>
-                            <input type="password" name="password" id="password" placeholder="Type your password" className="form-control mb-2"/>
-                            <Button text="Submit" color="btn-garden-dark"/>
-                    </div>
-                    <div className="col auth-form h-100">
-                        <h2>Register</h2>
-                            <label>Name</label>
-                            <input type="text" name="name" id="name" placeholder="Type your name" className="form-control mb-2"/>
-                            <label>Email</label>
-                            <input type="email" name="email" id="email1" placeholder="Type your email" className="form-control mb-2"/>
-                            <label>Password</label>
-                            <input type="password" name="password" id="password1"
-                            placeholder="Type a new password" className="form-control mb-2"/>
-                            <label>Confirm your password</label>
-                            <input type="password" name="password" id="password2" placeholder="Confirm your new password" className="form-control mb-2"/>
-                            <Button text="Submit" color="btn-garden-dark"/>
-                    </div>
-                </div>
-            </div>
-        </div>)
-  }
+      <div className="container">
+        <div className="row mt-5 justify-content-center">
+          <div className="col auth-form h-100">
+            <form>
+                <h2>Login</h2>
+                <label>Email</label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  placeholder="Type your email"
+                  className="form-control mb-2"
+                  required
+                />
+                <label>Password</label>
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="Type your password"
+                  className="form-control mb-2"
+                  required
+                />
+                <Button text="Submit" color="btn-garden-dark" />
+            </form>
+          </div>
+          <div className="col auth-form h-100">
+            <form action="">
+                <h2>Register</h2>
+                <label>Name</label>
+                <input
+                  type="text"
+                  name="name"
+                  id="name"
+                  placeholder="Type your name"
+                  className="form-control mb-2"
+                  required
+                />
+                <label>Email</label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email1"
+                  placeholder="Type your email"
+                  className="form-control mb-2"
+                  required
+                />
+                <label>Password</label>
+                <input
+                  type="password"
+                  name="password"
+                  id="password1"
+                  placeholder="Type a new password"
+                  className="form-control mb-2"
+                  required
+                />
+                <label>Confirm your password</label>
+                <input
+                  type="password"
+                  name="password"
+                  id="password2"
+                  placeholder="Confirm your new password"
+                  className="form-control mb-2"
+                  required
+                />
+                <Button text="Submit" color="btn-garden-dark" />
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
 
-  export default LandingPage
+export default LandingPage;
