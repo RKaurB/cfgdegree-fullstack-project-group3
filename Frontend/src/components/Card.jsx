8ac6222721c12ce6359d0ea31306a358faa3b2da
juliavolponi <juliadvolponi@gmail.com>
2026-06-07 14:30:02 +0100

    created reusable card component. added style to the card for a better layout. added image and title for a test

--- Frontend/src/components/Card.jsx
+++ Frontend/src/components/Card.jsx
@@ -1,6 +1,12 @@
+import Button from "./Button"
+
 function Card(props) {
-    return <div>
-        
+    return <div className="card">
+        <img className="card-img-top" src={props.image} alt={props.name}/>
+        <div className="card-body">
+            <h3 className="card-title">{props.name}</h3>
+            <Button text="Description" color="btn-garden-dark"/>
+        </div>
     </div>
 }
 
--- Frontend/src/pages/LandingPage.jsx
+++ Frontend/src/pages/LandingPage.jsx
@@ -1,3 +1,5 @@
 function LandingPage() {
     return <h1>Landing Page</h1>
   }
+
+  export default LandingPage
