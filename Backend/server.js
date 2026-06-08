5937ca8ccdb6fe0eeaf69d4dc32d2ee11e536034
rkaurb <63753021+RKaurB@users.noreply.github.com>
2026-06-08 05:09:16 +0100

    Add Perenual search service

--- Backend/server.js
+++ Backend/server.js
@@ -2,3 +2,18 @@ require("dotenv").config();
 
 // Test: verify .env variables loading correctly
 // console.log(process.env.PERENUAL_API_KEY);
+
+// Import search function
+const { searchPlants } = require("./Services/PerenualService");
+
+// Test Perenual search endpoint
+const testSearch = async () => {
+
+    const data = await searchPlants("carrot");
+
+    // console.log(data);
+    console.log(JSON.stringify(data, null, 2));
+
+};
+
+testSearch();
