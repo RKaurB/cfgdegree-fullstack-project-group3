6b43eb03cc30f1e3493c5c429cb0360645d3be37
rkaurb <63753021+RKaurB@users.noreply.github.com>
2026-06-11 00:00:42 +0100

    Test Perenual plant details endpoint and update research documentation

--- Backend/server.js
+++ Backend/server.js
@@ -10,24 +10,41 @@ const { searchPlants } = require("./Services/PerenualService");
 // Test Perenual search endpoint
 const testSearch = async () => {
   try {
-
     const data = await searchPlants("cilantro");
     // const data = await searchPlants("tarragon");
     // const data = await searchPlants("rosemary");
     // const data = await searchPlants("fennel");
     // const data = await searchPlants("dahlia");
     // const data = await searchPlants("qwerty123");
-
     // console.log(data);
     console.log(JSON.stringify(data, null, 2));
-
   } catch (error) {
-
     console.error(error);
-
   }
-
 };
-
 testSearch(); 
 */
+
+
+// Import plant details function
+const { getPlantDetails } = require("./Services/PerenualService");
+
+// Test Perenual plant details endpoint
+const testPlantDetails = async () => {
+
+    try {
+
+        // Test using valid id 2320 carrot
+        const data = await getPlantDetails(2320);
+        // console.log(data);
+        console.log(JSON.stringify(data, null, 2));
+
+    } catch (error) {
+
+        console.error(error);
+
+    }
+    
+}
+
+testPlantDetails();
