fc6325fecd6ae9111c52c00ae9f3b2082f8bd281
rkaurb <63753021+RKaurB@users.noreply.github.com>
2026-06-15 11:01:56 +0100

    Add Garden Buddy plant details functionality

--- Backend/testPlantService.js
+++ Backend/testPlantService.js
@@ -1,18 +1,30 @@
+
 require("dotenv").config();
 
-const { searchPlants } = require("./Services/PlantService");
+const { searchPlants, getPlantDetails } = require("./Services/PlantService");
 
 async function runTest() {
 
-    const results = await searchPlants("carrot");
+    console.log("\n===== TEST PLANT SEARCH =====\n");
+    const searchResults = await searchPlants("carrot");
     // Successfully returns search results
     // const results = await searchPlants("");
     // const results = await searchPlants(" ");
     // Returns error: Search term is required
     // const results = await searchPlants("test123");
     // Returns empty array
+    console.log(searchResults);
 
-    console.log(results);
+    console.log("\n===== TEST PLANT DETAILS =====\n");
+    const plantDetails = await getPlantDetails(2320);
+    console.log(plantDetails);
+    
+    console.log("\n===== TEST INVALID PLANT ID =====\n");
+    const invalidPlant = await getPlantDetails(99999999999);
+    // Returns Error: Unable to retrieve data from Perenual API
+    // const invalidPlant = await getPlantDetails("abc123")
+    // Returns Error: Unable to retrieve data from Perenual API
+    console.log(invalidPlant);
 
 }
 
