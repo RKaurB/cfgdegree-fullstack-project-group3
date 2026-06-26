1301fbad25b3c580849ef698308548b8b1658f14
omanoma <omanoma14@gmail.com>
2026-06-26 23:56:01 +0100

    bug fix with naming convinction

--- Frontend/src/pages/DashboardPage.jsx
+++ Frontend/src/pages/DashboardPage.jsx
@@ -30,12 +30,12 @@ function Dashboard() {
 
   // Added local state to hold data 
   // const [plants, setPlants] = useState([]); // to test the empty state
-  const [plants, setPlants] = useState(mockPlants); // uncomment this to back to the grid layout
+  const [plants, setPlants] = useState([]); // uncomment this to back to the grid layout
 
   // Search/filter plants 
   const [searchTerm, setSearchTerm] = useState("");
   const filteredPlants = plants.filter((plant) =>
-    plant.name.toLowerCase().includes(searchTerm.toLowerCase())
+    plant.commonName.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
   // task to be completed (2 plants)
