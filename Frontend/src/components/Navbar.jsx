083a77c5b44b1b7acafe206962b49b44489824ad
omanoma <omanoma14@gmail.com>
2026-06-27 22:55:28 +0100

    Add info for nav basr

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -2,13 +2,13 @@ import { useSelector, useDispatch } from "react-redux";
 import { clearUserInfo } from "../feature/UserSlice";
 import { SignOutAPI } from "../api/AuthAPI";
 import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 import logo from "../assets/plant.png";
 
 function NavBar() {
   const username = useSelector((state) => state.user.username);
   const dispatch = useDispatch();
   const nav = useNavigate();
-
   function handleSignOut() {
     SignOutAPI().then(async (res)=>{
       if(res.ok){
@@ -19,7 +19,7 @@ function NavBar() {
       }
     })
 
-  }
+  }  
 
   return (
     <nav className="navbar navbar-expand">
@@ -38,6 +38,25 @@ function NavBar() {
               <span className="nav-link">
                 Welcome to your garden, {username}!
               </span>
+              <button
+                onClick={()=>{nav("/dashboard");}}
+                className="nav-link btn btn-link ms-3"
+              >
+                Dashboard
+              </button>
+              <button
+                onClick={()=>{nav("/search");}}
+                className="nav-link btn btn-link ms-3"
+              >
+                Search Plant
+              </button>
+
+              <button
+                onClick={()=>{nav("/schedule");}}
+                className="nav-link btn btn-link ms-3"
+              >
+                Care-Schedule
+              </button>
               <button
                 onClick={handleSignOut}
                 className="nav-link btn btn-link ms-3"
