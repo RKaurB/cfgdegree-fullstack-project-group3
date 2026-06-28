458f02e576cdfbaa49581bd97a54b08fba436d1d
Hayley Reed <hayley_cox@outlook.com>
2026-06-28 11:49:20 +0100

    Tidied formatting

--- Backend/Tests/PlantSaved.test.js
+++ Backend/Tests/PlantSaved.test.js
@@ -46,40 +46,6 @@ test("User failed to add plant to database",async ()=>{
     expect(await result.status).toBe(400)
    });
 
-// jest.mock("../Scheme/PlantSaved")
-
-// const dataFromAPI = require("../Scheme/PlantSaved")
-
-// test("Plant deleted from user profile", async ()=>{
-//     dataFromAPI.mockResolvedValue({ data: "Mock plant" });
-
-//     const result5 = await dataFromAPI();
-//     expect (result).toEqual({ data: "Mock plant" });
-// });
-
-
-// test("Plant deleted from user profile", async ()=>{
-//     const result5 = jest.fn().mockResolvedValue({ success:true });
-//     const response = await result5();
-    
-//     await expect(response.sucess).toBe(true);
-
-// });
-    
-
-// Test if the user failed to add a plant to the database - DELETE
-// test("Test if new plant was not added", async () => {
-//     const result3 = jest.fn()
-
-//     result3.mockResolvedValue({ data: "Mocked Plant" });
-    
-//     const result4 = await plantSaved();
-//     expect(await result4).toEqual({ data: "Mocked Plant" });
-// });
-
-
-
-
 // Test if we can get a users plant ID
 test("Get users list of plants", async ()=>{
     const result = await account.LoginIn("test12@gi.com","G/2%,J];Muge<<8M")
@@ -90,7 +56,6 @@ test("Get users list of plants", async ()=>{
 });
 
 // Test if plant saved successfully to user profile
-
 test("New plant saved successfully to database",async ()=>{
     let g = {
         "plantApiId": 2320,
